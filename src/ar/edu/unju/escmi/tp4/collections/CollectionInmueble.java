4c6e5b444b2e0ca2195f91a7a59818088254eb5e
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-09-27 01:17:31 -0300

    avances

--- src/ar/edu/unju/escmi/tp4/collections/CollectionInmueble.java
+++ src/ar/edu/unju/escmi/tp4/collections/CollectionInmueble.java
@@ -1,5 +1,22 @@
 package ar.edu.unju.escmi.tp4.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.escmi.tp4.dominio.Inmueble;
+
 public class CollectionInmueble {
+	
+    public static List<Inmueble> inmuebles = new ArrayList<>();
+
+    public static void agregarInmueble(Inmueble inmueble) {
+        inmuebles.add(inmueble);
+    }
+    
+    public static void mostrarInmuebles() {
+        for (Inmueble inmueble : inmuebles) {
+            inmueble.mostrarDatos();
+        }
+    }
 
 }
