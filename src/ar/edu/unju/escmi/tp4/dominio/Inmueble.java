8e4bf415ae7c52136145561902d3e9481f811338
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-09-26 22:25:27 -0300

    Avance en Inmueble_GuariTatiana

--- src/ar/edu/unju/escmi/tp4/dominio/Inmueble.java
+++ src/ar/edu/unju/escmi/tp4/dominio/Inmueble.java
@@ -1,5 +1,20 @@
 package ar.edu.unju.escmi.tp4.dominio;
 
 public abstract class Inmueble {
+	protected int codigo;
+	protected boolean disponible; 
+	
+	
+	public Inmueble() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Inmueble(int codigo, boolean disponible) {
+		// TODO Auto-generated constructor stub
+		this.codigo = codigo;
+		this.disponible = disponible;
+	}
+	
+	public abstract void mostrarDatos();
 
 }
