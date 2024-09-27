ea8d38ee47136948d2c99fa8ff0df6d5cbcbe1e9
PC <PC@DESKTOP-H5EFDLF>
2024-09-26 21:51:38 -0300

    Corregida clase Cliente. Valerio Sebastian

--- src/ar/edu/unju/escmi/tp4/dominio/Cliente.java
+++ src/ar/edu/unju/escmi/tp4/dominio/Cliente.java
@@ -7,8 +7,23 @@ public class Cliente {
 	public String telefono;
 	public String direccion;
 	public String email;
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Cliente(String nombre, String apellido, String dni, String telefono, String direccion, String email) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.email = email;
+	}
+
 	public static void mostrarDatos() {
-		
 	};
+	
+	
 }
 
