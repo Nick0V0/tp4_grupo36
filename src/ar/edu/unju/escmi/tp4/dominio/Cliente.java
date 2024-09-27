e94a9c8dd0269621cf557fd0244b2a588a0412b9
PC <PC@DESKTOP-H5EFDLF>
2024-09-26 21:39:08 -0300

    Agregada la clase Cliente.Valerio Sebastian

---
+++ src/ar/edu/unju/escmi/tp4/dominio/Cliente.java
@@ -0,0 +1,14 @@
+package ar.edu.unju.escmi.tp4.dominio;
+
+public class Cliente {
+	public String nombre;
+	public String apellido;
+	public String dni;
+	public String telefono;
+	public String direccion;
+	public String email;
+	public static void mostrarDatos() {
+		
+	};
+}
+
