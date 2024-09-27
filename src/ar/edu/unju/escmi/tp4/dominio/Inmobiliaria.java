bbe31629603c72acbb693e59099921f97f5dd0e5
PC <PC@DESKTOP-H5EFDLF>
2024-09-26 21:47:15 -0300

    Agregada la Clase Inmobiliaria.Valerio Sebastian

---
+++ src/ar/edu/unju/escmi/tp4/dominio/Inmobiliaria.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.escmi.tp4.dominio;
+
+public class Inmobiliaria {
+	public String nombre;
+	public String telefono;
+	public String direccion;
+	
+	public Inmobiliaria() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Inmobiliaria(String nombre, String telefono, String direccion) {
+		super();
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
+	public static void mostrarDatos() {
+	};
+	
+}
