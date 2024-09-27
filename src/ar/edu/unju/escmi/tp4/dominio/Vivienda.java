8b1f59e5571b21f021582f43a6f40dcd2a543645
garbarino <garbarino@LAPTOP-88IL2DC7>
2024-09-26 22:44:35 -0300

    avance clase vivienda_Guari

--- src/ar/edu/unju/escmi/tp4/dominio/Vivienda.java
+++ src/ar/edu/unju/escmi/tp4/dominio/Vivienda.java
@@ -1,5 +1,32 @@
 package ar.edu.unju.escmi.tp4.dominio;
 
 public class Vivienda extends Inmueble {
+	
+
+	private String direccion;
+	private int habitaciones;
+	private double precioAlquiler;
+	
+	public Vivienda() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+    public Vivienda(int codigo, boolean disponible, String direccion, int habitaciones, double precioAlquiler) {
+        super(codigo, disponible);
+        this.direccion = direccion;
+        this.habitaciones = habitaciones;
+        this.precioAlquiler = precioAlquiler;
+    }
+	
+	@Override
+	public void mostrarDatos() {
+		// TODO Auto-generated method stub
+		System.out.println("Vivienda: " + codigo);
+		System.out.println("Direccion: " + direccion);
+		System.out.println("Habitaciones: " + habitaciones);
+		System.out.println("Precio Venta $: " + precioAlquiler);
+		System.out.println("Estado: " + disponible);
+	}
 
 }
